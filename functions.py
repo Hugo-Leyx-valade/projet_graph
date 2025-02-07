400487af72cff69e8e53ef8944a022eb2427d05a
hugol <hugo.leyx-valade@efrei.net>
2025-02-07 20:27:47 +0100

    j'ai ajouté les alphas

--- functions.py
+++ functions.py
@@ -4,9 +4,17 @@ def graph_import(link):
     f= open(link,'r')
     actual_line = f.readline()
     graph = []
+    alpha = [0, 0]
     while actual_line != "":
-        graph.append(list(map(int,actual_line.split())))
-        actual_line = f.readline()
+        if len(list(map(int,actual_line.split()))) == 2 :
+            line_to_insert = list(map(int,actual_line.split())) + [0]
+            graph.append(line_to_insert)
+            actual_line = f.readline()
+        else:
+            graph.append(list(map(int,actual_line.split())))
+            actual_line = f.readline()
+
+    graph.insert(0,alpha)
 
     for line in graph:
         print(line)
@@ -24,14 +32,14 @@ def print_graph_to_matrix_of_values(liste_adjacence):
     # Remplir la matrice avec les valeurs données
     for i in range(n):
         for j in range(len(liste_adjacence[i])-2):
-            matrice[int(liste_adjacence[i][j+2])-1][i] = str(liste_adjacence[i][j+2])  # Stocker en tant que chaîne pour l'affichage
+            matrice[int(liste_adjacence[i][j+2])][i] = str(liste_adjacence[i][j+2])  # Stocker en tant que chaîne pour l'affichage
 
     # Affichage de l'en-tête des colonnes
-    header = "  " + " ".join(str(i+1) for i in range(n))
+    header = "  " + " ".join(str(i) for i in range(n))
     print(header)
 
     # Affichage des lignes
     for i in range(n):
-        row = f"{i+1} " + " ".join(matrice[i][j] if j < len(matrice[i]) else '*' for j in range(n))
+        row = f"{i} " + " ".join(matrice[i][j] if j < len(matrice[i]) else '*' for j in range(n))
         print(row)
 
