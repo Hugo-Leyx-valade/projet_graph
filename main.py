269a93afbedb5d639aee40b758c1516fec0bec6b
Max <max.bortolotti@efrei.net>
2025-03-30 12:53:34 +0200

    Ajout des traces d'exec pour tous graphes sauf 1 et 12.

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from functions import *
 print("Hello")
 
 def main():
-    graph = graph_import("graphe2.txt") #Ajouter table*.txt
+    graph = graph_import("table 14.txt") #Ajouter table*.txt
     if graph == -1:
         print("il y a au moins un arc à valeur négative !")
         return
